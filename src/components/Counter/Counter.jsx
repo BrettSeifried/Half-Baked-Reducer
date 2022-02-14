b1b23d80932bb545fb54dfb86afe7f876695a3a0
Brett Seifried <seifriedbrett@gmail.com>
2022-02-14 12:36:05 -0800

    working increment and decrement

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,47 +1,90 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 
 const pinkRGB = `rgb(236, 72, 153)`
 
+const initialItems = { count: 0 }
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'increment': {
+      return {
+        count: state.count + 1,
+      }
+    }
+    case 'decrement': {
+      return {
+        count: state.count - 1,
+      }
+    }
+    case 'reset': {
+      return {
+        count: state.count initialItems,
+      }
+    }
+    default: {
+      throw Error(`unknown action: ${action.type}`)
+    }
+  }
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(countReducer, initialItems)
   const [currentColor, setCurrentColor] = useState(pinkRGB)
 
+  const hanldeIncrement = (count) => {
+    dispatch({
+      type: 'increment',
+    })
+  }
+
+  const handleDecrement = (count) => {
+    dispatch({
+      type: 'decrement',
+    })
+  }
+
+  const handleReset = (count) => {
+    dispatch({
+      type: 'reset',
+    })
+  }
+
   useEffect(() => {
-    if (count === 0) {
+    if (state.count === 0) {
       setCurrentColor(pinkRGB)
     }
 
-    if (count > 0) {
+    if (state.count > 0) {
       setCurrentColor(`rgb(52, 211, 153)`)
     }
 
-    if (count < 0) {
+    if (state.count < 0) {
       setCurrentColor(`rgb(239, 68, 68)`)
     }
-  }, [count])
+  }, [state.count])
 
-  const increment = () => {
-    setCount((prevState) => prevState + 1)
-  }
+  // const increment = () => {
+  //   setCount((prevState) => prevState + 1)
+  // }
 
-  const decrement = () => {
-    setCount((prevState) => prevState - 1)
-  }
+  // const decrement = () => {
+  //   setCount((prevState) => prevState - 1)
+  // }
 
-  const reset = () => {
-    setCount(0)
-  }
+  // const reset = () => {
+  //   setCount(0)
+  // }
 
   return (
     <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
       <h1 className="mb-5" style={{ color: currentColor }}>
-        {count}
+        {state.count}
       </h1>
       <div className="flex w-1/2 justify-around">
         <button
           className="text-green-400 border-2 border-green-400 p-3"
           type="button"
-          onClick={increment}
+          onClick={hanldeIncrement}
           aria-label="increment"
         >
           Increment
@@ -49,16 +92,17 @@ export default function Counter() {
         <button
           className="text-red-500 border-2 border-red-500 p-2"
           type="button"
-          onClick={decrement}
+          onClick={handleDecrement}
           aria-label="decrement"
         >
           Decrement
         </button>
+
         <button
           className="text-pink-500 border-2 border-pink-500 p-2"
           type="button"
           aria-label="reset"
-          onClick={reset}
+          onClick={handleReset}
         >
           Reset
         </button>
